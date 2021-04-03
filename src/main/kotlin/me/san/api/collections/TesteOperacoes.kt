969521a3cf7affa118ab7d7054b45d37d25a2839
Santiago Silvestre <silvestre.santiago732@gmail.com>
2021-04-03 17:21:11 -0300

    Update in file TesteOperacoes

--- src/main/kotlin/me/san/api/collections/TesteOperacoes.kt
+++ src/main/kotlin/me/san/api/collections/TesteOperacoes.kt
@@ -14,6 +14,12 @@ fun main() {
 
     val salariosMaior2500 = salarios.filter { it < 2000.0 }
     salariosMaior2500.forEach { println(it) }
+    separar()
+    println(salarios.count{it in 2000.0..5000.0})
+    separar()
+    println(salarios.find{ it == 6.0})
+    separar()
+    println(salarios.any{ it == 1940.5})
 }
 fun separar() {
     println("---------------")
